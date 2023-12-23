e1d3498b42a68fff6d3f732133ecd94adcb98a82
Martin Bell <martin.bell@mac.com>
2023-12-23 10:15:12 +0000

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Martin Bell on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
